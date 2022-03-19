3c89fceedfe1ace6ae4760cd1dafde4e78c7b235
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-03-19 14:11:55 +0330

    refactor: run eslint

--- src/VueRouterLoading.vue
+++ src/VueRouterLoading.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    <transition name="fade">
+    <Transition name="fade">
       <div
         v-if="isStarted"
         :style="progressBarStyle"
-        :class="['progressbar', { 'isPaused': isPaused }, { 'isFinished': isFinished }, { 'rtl': rtl }, { 'showOverly': showOverly }]"
+        :class="[
+          'progressbar',
+          { isPaused: isPaused },
+          { isFinished: isFinished },
+          { rtl: rtl },
+          { showOverly: showOverly }
+        ]"
       >
-        <span class="shadow" v-if="endLineMode == 'shadow'" :style="shadowStyle"></span>
-        <span class="circle" v-else-if="endLineMode == 'circle'" :style="circleStyle"></span>
+        <span
+          v-if="endLineMode == 'shadow'"
+          class="shadow"
+          :style="shadowStyle"
+        />
+        <span
+          v-else-if="endLineMode == 'circle'"
+          class="circle"
+          :style="circleStyle"
+        />
       </div>
-    </transition>
+    </Transition>
   </div>
 </template>
 
@@ -19,28 +33,28 @@ export default {
 
   props: {
     color: {
-      default: '#41b883'
+      default: "#41b883"
     },
     height: {
       default: 2
     },
     position: {
-      default: 'top'
+      default: "top"
     },
     rtl: {
       default: false
     },
     endLineMode: {
-      default: 'shadow'
+      default: "shadow"
     },
     shadowColor: {
-      default: '#41b883'
+      default: "#41b883"
     },
     shadowOpacity: {
-      default: '0.9'
+      default: "0.9"
     },
     circleColor: {
-      default: '#41b883'
+      default: "#41b883"
     },
     showOverly: {
       default: true
@@ -58,85 +72,93 @@ export default {
       return {
         [this.position]: 0,
         background: this.color,
-        height: `${this.height}px`,
-      }
+        height: `${this.height}px`
+      };
     },
 
     shadowStyle() {
-      let shadowColor = (this.isHex(this.shadowColor)) ?  this.hexToRgb(this.shadowColor) : this.shadowColor
+      let shadowColor = this.isHex(this.shadowColor)
+        ? this.hexToRgb(this.shadowColor)
+        : this.shadowColor;
       return {
-        'box-shadow': `0 0 10px rgba(${shadowColor}, ${this.shadowOpacity})`
-      }
+        "box-shadow": `0 0 10px rgba(${shadowColor}, ${this.shadowOpacity})`
+      };
     },
 
     circleStyle() {
       return {
         background: this.circleColor,
         width: `${+this.height * 3}px`,
-        height: `${+this.height * 3}px`,
-      }
-    }
-  },
-
-  mounted() {
-    this.$router.beforeEach((from, to, next) => {
-      this.start()
-      next()
-    })
-  },
-
-  methods: {
-    start() {
-      if (this.isStarted) this.destroy();
-      this.isStarted = true
-      setTimeout(() => {
-        if (this.isStarted && !this.isFinished) this.isPaused = true
-      }, 8500)
-    },
-
-    finish() {
-      this.isFinished = true
-      this.isPaused = false
-    },
-
-    destroy() {
-      this.isStarted = false
-      this.isFinished = false
-      this.isPaused = false
-    },
-
-    hexToRgb(hex) {
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
-    },
-
-    isHex(str) {
-      return str.match(/^#[a-f0-9]{6}$/i) !== null;
+        height: `${+this.height * 3}px`
+      };
     }
   },
 
   watch: {
     isStarted(val) {
-      if (val) setTimeout(() => {
-        this.$el.addEventListener("animationend", this.destroy);
-      }, 500)
+      if (val)
+        setTimeout(() => {
+          this.$el.addEventListener("animationend", this.destroy);
+        }, 500);
     },
 
     $route() {
-      setTimeout(() => this.finish(), 500)
+      setTimeout(() => this.finish(), 500);
+    }
+  },
+
+  mounted() {
+    this.$router.beforeEach((from, to, next) => {
+      this.start();
+      next();
+    });
+  },
+
+  methods: {
+    start() {
+      if (this.isStarted) this.destroy();
+      this.isStarted = true;
+      setTimeout(() => {
+        if (this.isStarted && !this.isFinished) this.isPaused = true;
+      }, 8500);
+    },
+
+    finish() {
+      this.isFinished = true;
+      this.isPaused = false;
+    },
+
+    destroy() {
+      this.isStarted = false;
+      this.isFinished = false;
+      this.isPaused = false;
+    },
+
+    hexToRgb(hex) {
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result
+        ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(
+            result[3],
+            16
+          )}`
+        : null;
+    },
+
+    isHex(str) {
+      return str.match(/^#[a-f0-9]{6}$/i) !== null;
     }
   }
-
 };
 </script>
 
-
 <style lang="scss" scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 }
 
@@ -166,7 +188,7 @@ export default {
   &.showOverly {
     &::after {
       display: none;
-      content: '';
+      content: "";
       position: absolute;
       top: 0;
       left: 0%;
@@ -218,7 +240,8 @@ export default {
   left: unset;
   right: 0;
 
-  .shadow, .circle {
+  .shadow,
+  .circle {
     right: unset;
     left: 0;
   }
